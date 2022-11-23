f1b6a9a2cf28b17d51a1c41f91918f2461e8ff53
Daniela Lima <89270964+mrslima@users.noreply.github.com>
2022-11-23 19:10:33 -0300

    Update programa_1.c

--- programa_1.c
+++ programa_1.c
@@ -2,8 +2,7 @@
 
 int main(int argc, char *argv[]) {
 	
-	// Verifica se os parâmetros foram informados corretamente.
-    if (!argsVerify(argc)) return 1;
+   	if (!argsVerify(argc)) return 1;
 
 
 	char *in_path = argv[1];
@@ -12,8 +11,6 @@ int main(int argc, char *argv[]) {
 	type_actors *arr_actors;
 	arr_actors = malloc(sizeof(type_actors) * ARRAY_SIZE);
 
-
-	// Lê o conteúdo do arquivo .txt e armazena num array de struct
 	if (readTextFile(in_path, arr_actors)) {
 		printf("[%s]\tSUCESSO: leitura bem sucedida.\n\n", in_path);
 	} else {
@@ -21,7 +18,6 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
-	// Pega o conteúdo do array de strutc e escreve (em binário) num arquivo .bin
 	if (writeBinFile(out_path, arr_actors, ARRAY_SIZE)) {
 		printf("[%s] -> [%s]\tSUCESSO: escrita bem sucedida.\n\n", in_path, out_path);
 	} else {
@@ -29,7 +25,6 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
-	//  Limpa o array de struct
 	free(arr_actors);
 
 
